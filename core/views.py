8bf9d35e3441552aa4f612753794b5eccb7c3680
ClaudiaHazard <claudia.hazard.14@sansano.usm.cl>
2020-03-06 05:24:55 -0300

    complete

--- core/views.py
+++ core/views.py
@@ -20,33 +20,76 @@ def base(request):
             plazo = int(request.POST['plazo'])
             print("PLAZO ES: ",plazo)
             both=ano+'/'+mes
-            response = requests.get('https://api.sbif.cl/api-sbifv3/recursos_api/tmc/posteriores/'+both+'?apikey=9f253b4442c3edf7b2f54a3e7b7e6ab6a8163bdb&formato=json')
+            response = requests.get('https://api.sbif.cl/api-sbifv3/recursos_api/tmc/'+both+'?apikey=9f253b4442c3edf7b2f54a3e7b7e6ab6a8163bdb&formato=json')
             data=response.json()
             values=data['TMCs']
             context['val']=data['TMCs']
+            print(values)
             for i in values:
                 print(i['Titulo'])
-                if (plazo >= 90) and  (('90 días o más' in i['Titulo']) or ('Menores a un año' in i['Titulo'])):
-                    if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 200) and ('Superiores al equivalente de 200' in i['SubTitulo'] or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['Subtitulo']))):
-                        context['val'] = i['Valor']
-                elif(plazo < 90) and (('menores de 90 días' in i['Titulo']) or ('Menores a un año'in i['Titulo'])):
-                    if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 200) and ('Superiores al equivalente de 200' in i['SubTitulo'] or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['Subtitulo']))):
-                        context['val'] = i['Valor']
-                else:
-                    if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'])):
-                        context['val'] = i['Valor']
-                    elif ((monto > 200) and ('Superiores al equivalente de 200' in i['SubTitulo'] or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['Subtitulo']))):
-                        context['val'] = i['Valor']
+                if(i['Titulo']!=None):
+                    if(plazo>=365 and ('De un año o más' in i['Titulo'] or 'De un año o más' in i['SubTitulo'])):
+                        if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'] or 'Superiores al equivalente de 2000' in i['SubTitulo']) or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 200) and ('Superiores al equivalente de 200 ' in i['SubTitulo'] )):
+                            context['val'] = i
+                        elif ((monto < 50) and ('Inferiores' in i['SubTitulo'] and ('50' or '200' or '2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 200) and ('Inferiores' in i['SubTitulo'] and ('5.000' or '200' or '2.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 2000) and ('Inferiores' in i['SubTitulo'] and ('2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 5000) and ('Inferiores' in i['SubTitulo'] and ('5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                    elif (plazo >= 90) and  (('90 días o más' in i['Titulo']) or ('Menores a un año' in i['Titulo'])):
+                        if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'] or 'Superiores al equivalente de 2000' in i['SubTitulo']) or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 200) and ('Superiores al equivalente de 200 ' in i['SubTitulo'] )):
+                            context['val'] = i
+                        elif ((monto < 50) and ('Inferiores' in i['SubTitulo'] and ('50' or '200' or '2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 200) and ('Inferiores' in i['SubTitulo'] and ('5.000' or '200' or '2.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 2000) and ('Inferiores' in i['SubTitulo'] and ('2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 5000) and ('Inferiores' in i['SubTitulo'] and ('5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                    elif(plazo < 90) and (('menores de 90 días' in i['Titulo']) or ('Menores a un año'in i['Titulo']) or 'menos de 90 ' in i['Titulo']):
+                        if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'] or 'Superiores al equivalente de 2000' in i['SubTitulo']) or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 200) and ('Superiores al equivalente de 200 ' in i['SubTitulo'] )):
+                            context['val'] = i
+                        elif ((monto < 50) and ('Inferiores' in i['SubTitulo'] and ('50' or '200' or '2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 200) and ('Inferiores' in i['SubTitulo'] and ('5.000' or '200' or '2.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 2000) and ('Inferiores' in i['SubTitulo'] and ('2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 5000) and ('Inferiores' in i['SubTitulo'] and ('5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                    elif((plazo < 90 and ((' días o más' not in i['Titulo']) and ('año o más' not in i['Titulo']))) or ('Operaciones reajustables en moneda nacional' in i['Titulo']and ('año o más' not in i['SubTitulo']))):
+                        if ((monto > 5000) and ('Superiores al equivalente de 5.000' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 2000) and ('Superiores al equivalente de 2.000' in i['SubTitulo'] or 'Superiores al equivalente de 2000' in i['SubTitulo']) or ('Inferiores o iguales al equivalente de 2000 unidades de fomento' in i['SubTitulo'])):
+                            context['val'] = i
+                        elif ((monto > 200) and ('Superiores al equivalente de 200 ' in i['SubTitulo'] )):
+                            context['val'] = i
+                        elif ((monto < 50) and ('Inferiores' in i['SubTitulo'] and ('50' or '200' or '2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 200) and ('Inferiores' in i['SubTitulo'] and ('5.000' or '200' or '2.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 2000) and ('Inferiores' in i['SubTitulo'] and ('2.000' or '5.000' in i['SubTitulo']))):
+                            context['val'] = i
+                        elif ((monto < 5000) and ('Inferiores' in i['SubTitulo'] and ('5.000' in i['SubTitulo']))):
+                            context['val'] = i
+            context['tmc']='El TMC corresponde a '+context['val']['Valor']+'%.'
+            context['val']='Sujeto a '+context['val']['Titulo']+', '+context['val']['SubTitulo']+'.' 
 
     else: 
         form = TMCForm() 
